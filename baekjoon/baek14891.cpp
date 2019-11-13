a643111c6cb1f278abd1e3c02166667accbc081b
YUJIN511 <43337244+YUJIN511@users.noreply.github.com>
2019-11-13 15:29:26 +0900

    Rename baek14891.cpp to baekjoon/baek14891.cpp

--- baekjoon/baek14891.cpp
+++ baekjoon/baek14891.cpp
@@ -8,44 +8,44 @@ void rotation(int num, int dir, char T[4][10], int lock) {
 		temp[i] = T[num - 1][i];
 	}
 
-	if (dir == 1) { // ½Ã°è¹æÇâ
+	if (dir == 1) { // ì‹œê³„ë°©í–¥
 		for (int i = 1; i < 8; i++) {
 			T[num - 1][i] = temp[i - 1];
 		}
 		T[num - 1][0] = temp[7];
 	}
-	else { // ¹Ý½Ã°è¹æÇâ
+	else { // ë°˜ì‹œê³„ë°©í–¥
 		for (int i = 0; i < 7; i++) {
 			T[num - 1][i] = temp[i + 1];
 		}
 		T[num - 1][7] = temp[0];
 	}
 
-	if (num == 1) { // 1¹ø Åé´Ï¹ÙÄû
-		if (temp[2] == T[1][6]) { // 1¹ø ±Ø == 2¹ø ±Ø
+	if (num == 1) { // 1ë²ˆ í†±ë‹ˆë°”í€´
+		if (temp[2] == T[1][6]) { // 1ë²ˆ ê·¹ == 2ë²ˆ ê·¹
 			return;
 		}
-		else if (lock != 2) { // ´Ù¸¥ ±Ø
+		else if (lock != 2) { // ë‹¤ë¥¸ ê·¹
 			rotation(2, dir*-1, T, 1);
 		}
 	}
-	else if (num == 4) { // 4¹ø Åé´Ï¹ÙÄû
-		if (temp[6] == T[2][2]) { // 4¹ø ±Ø == 3¹ø ±Ø
+	else if (num == 4) { // 4ë²ˆ í†±ë‹ˆë°”í€´
+		if (temp[6] == T[2][2]) { // 4ë²ˆ ê·¹ == 3ë²ˆ ê·¹
 			return;
 		}
-		else if (lock != 3) { // ´Ù¸¥ ±Ø
+		else if (lock != 3) { // ë‹¤ë¥¸ ê·¹
 			rotation(3, dir*-1, T, 4);
 		}
 	}
-	else { // ³ª¸ÓÁö Åé´Ï¹ÙÄû
-		if (lock == 0 && temp[6] != T[num - 2][2] && temp[2] != T[num][6]) { // ¿ÞÂÊ ±Ø ¿À¸¥ÂÊ ±Ø ¸ðµÎ ´Ù¸¥Áö
+	else { // ë‚˜ë¨¸ì§€ í†±ë‹ˆë°”í€´
+		if (lock == 0 && temp[6] != T[num - 2][2] && temp[2] != T[num][6]) { // ì™¼ìª½ ê·¹ ì˜¤ë¥¸ìª½ ê·¹ ëª¨ë‘ ë‹¤ë¥¸ì§€
 			rotation(num - 1, dir*-1, T, num);
 			rotation(num + 1, dir*-1, T, num);
 		}
-		else if (lock != num - 1 && temp[6] != T[num - 2][2]) { // ¿ÞÂÊ ±Ø¸¸ ´Ù¸¦¶§
+		else if (lock != num - 1 && temp[6] != T[num - 2][2]) { // ì™¼ìª½ ê·¹ë§Œ ë‹¤ë¥¼ë•Œ
 			rotation(num - 1, dir*-1, T, num);
 		}
-		else if (lock != num + 1 && temp[2] != T[num][6]) { // ¿À¸¥ÂÊ ±Ø¸¸ ´Ù¸¦¶§
+		else if (lock != num + 1 && temp[2] != T[num][6]) { // ì˜¤ë¥¸ìª½ ê·¹ë§Œ ë‹¤ë¥¼ë•Œ
 			rotation(num + 1, dir*-1, T, num);
 		}
 	}
@@ -69,7 +69,7 @@ int main() {
 	for (int i = 0; i < count; i++) {
 		cin >> num;
 		cin >> dir;
-		rotation(num, dir, T, 0);// È¸Àü
+		rotation(num, dir, T, 0);// íšŒì „
 	}
 
 	cout << (T[0][0] - '0') * 1 + (T[1][0] - '0') * 2 + (T[2][0] - '0') * 4 + (T[3][0] - '0') * 8 << endl;
